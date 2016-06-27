5e188a326942a4eb944cf8c19d4ec41753a6d474
Rena Goldsmith <rena.kg@gmail.com>
2016-06-28 11:56:23 +1200

    start a new view

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 var xhr = require('xhr')
 var greeting = require('./views/greeting.hbs')
 
-var endpoint = 'https://api.wheretheiss.at/v1/satellites'
+var endpoint = 'https://api.wheretheiss.at/v1/satellites/25544'
 
 xhr.get(endpoint, function (err, data) {
   if (err) {
     console.error(err)
   }
-
   var data = JSON.parse(data.body)
-  // console.log(data, 'should be an object I can use')
-  // Replace 'Space' below with the response
+  //render the name
+  var uppercase = data.name.toUpperCase()
   var target = document.getElementsByTagName('main')[0]
-  target.innerHTML = greeting(data[0])
+  target.innerHTML = greeting({name: uppercase})
+
+
 })
