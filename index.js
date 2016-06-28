b07ef176f8ac4d5130a108768303b116a1dfe3a8
Rena Goldsmith <rena.kg@gmail.com>
2016-06-28 16:07:06 +1200

    add event listener

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
 var xhr = require('xhr')
 var greeting = require('./views/greeting.hbs')
+var satellites = require('./views/satellites.hbs')
 
 var endpoint = 'https://api.wheretheiss.at/v1/satellites/25544'
 
-xhr.get(endpoint, function (err, data) {
+xhr.get(endpoint, function(err, data) {
   if (err) {
     console.error(err)
   }
   var data = JSON.parse(data.body)
-  //render the name
+  console.log(data, 'this is the data')
+  //render the name on greeting hbs
   var uppercase = data.name.toUpperCase()
   var target = document.getElementsByTagName('main')[0]
   target.innerHTML = greeting({name: uppercase})
-
-
+  //render data on satellites
+  var addInfo = document.getElementsByTagName('button')[0]
+  var coords = document.getElementById('coords')
+  addInfo.addEventListener("click", function() {
+    coords.innerHTML = satellites(data)
+  })
 })
